5b9ca6bc6b981f0711885234aff9f9e0d4385c61
Mohammad Newton <newtonombese1@gmail.com>
2024-08-03 17:35:43 +0300

    Setting up the pizza project

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
+const Home = () => {
+  return (
+    <div className="home container">
+      <motion.h2 animate={{ fontSize: 50 }}>
+        Welcome to Pizza Joint
+      </motion.h2>
+      <Link to="/base">
+        <button>
+          Create Your Pizza
+        </button>
+      </Link>
+    </div>
+  )
+}
+
+export default Home;
